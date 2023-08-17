706695e41d6b9f71125e59b5445acc45435a5dc9
Tony Osunde <112965272+SirTonero@users.noreply.github.com>
2023-08-18 00:30:04 +0100

    fix route bug

--- vpc-module/main.tf
+++ vpc-module/main.tf
@@ -53,7 +53,7 @@ resource "aws_subnet" "public_subnet_az2" {
 resource "aws_route_table" "public_route_table" {
    vpc_id = aws_vpc.vpc
 
-   route = {
+   route {
       cidr_block = "0.0.0.0/0"
       gateway_id = aws_internet_gateway.internet_gateway.id
    }
